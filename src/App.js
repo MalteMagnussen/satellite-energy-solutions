61fe3baedd8cc29bc5d13588d9214f792b9b66f9
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-11-16 11:42:54 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Container } from "react-bootstrap";
+import NavBar from "./components/NavBar.jsx";
+import { HashRouter, Link, Route } from "react-router-dom";
+import HomePage from "./components/HomePage.jsx";
+import TeamPage from "./components/TeamPage.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar></NavBar>
+      <Container>
+        <br></br>
+        <HashRouter>
+          <Route path="/team" component={TeamPage} />
+          <Route exact path="/" component={HomePage} />
+        </HashRouter>
+      </Container>
     </div>
   );
 }
---
+++ src/components/TeamPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <>
+      <h1>This is /team TeamPage</h1>
+    </>
+  );
+};
+
+export default TeamPage;
