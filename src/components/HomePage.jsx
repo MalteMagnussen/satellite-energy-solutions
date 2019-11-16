fa0e515c72e8330a0a6236c015adee23c78b9150
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-11-16 17:00:28 +0100

    Added Execute Summary

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
     <>
       <Jumbotron>
         <h1>Satellite Energy Solutions</h1>
-        <p>Energy Price Forecasting.</p>
+        <JumboTronText />
         <p>
           <Button variant="primary" onClick={learnMore}>
             Learn more
@@ -18,6 +18,22 @@ const HomePage = () => {
         </p>
       </Jumbotron>
     </>
+  );
+};
+
+const JumboTronText = () => {
+  return (
+    <p>
+      A total of €65bn was invested in 2018 on Europe level by the wind energy
+      sector alone. To accurately facilitate auctions and examine business cases
+      for the more than 420 wind turbines build that year, large amounts of data
+      and analysis is needed. Using the Copernicus data set we aim to provide
+      better forecasts of what production and electricity prices investors and
+      other actors can expect. We will also provide a user-friendly platform to
+      access and retrieve the data the customer needs in a user-friendly format,
+      along with visualized data sets such as screening maps for wind speed or
+      production.
+    </p>
   );
 };
 
