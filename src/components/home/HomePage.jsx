7a5a2ab8c30c9dd119187ee2f921bf950ef0174e
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-11-19 16:48:18 +0100

    FrontPage ready
    Missing Links on Cards

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-import { Container, Card } from "react-bootstrap";
+import { Container, Card, CardDeck } from "react-bootstrap";
 import LinkButton from "../util/LinkButton.jsx";
 import windTurbine from "./img/cover.png";
+import satellite from "./img/satellite.jpg";
+import power from "./img/power.jpg";
+import windTurbineBelow from "./img/windTurbineBelow.jpg";
+import screeningMap from "./img/ScreeningMap.png";
+
+const cardImageStyle = {
+  height: "400px"
+};
 
 const HomePage = () => {
   return (
@@ -13,30 +21,73 @@ const HomePage = () => {
       <br></br>
       <br></br>
       <br></br>
-      <div>
-        <Card className="bg-dark text-white">
-          <Card.Img src={windTurbine} alt="Card image" />
-          <Card.ImgOverlay style={{ marginTop: "130px" }}>
-            <Card.Title>
-              <h3>
-                <b>
-                  Get better data and projections for your renewable business
-                  case needs.
-                </b>
-              </h3>
-            </Card.Title>
-            <Card.Text>
+      <WindTurbineCard />
+      <br></br>
+      <br></br>
+      <DiscoverOurServices />
+      <br></br>
+      <MyFrontPageCardDeck />
+      <br></br>
+      <Footer />
+    </>
+  );
+};
+
+const Footer = () => {
+  const topSpace = {
+    marginTop: "50px"
+  };
+  return (
+    <>
+      <Card bg="dark" text="white">
+        <Card.Header>
+          <h1 style={topSpace}>Want to know more, or get involved?</h1>
+        </Card.Header>
+        <Card.Body>
+          <Card.Title style={topSpace}>
+            We are still new and eager to get feedback and new collaborations.
+            <br></br>
+            If your interest is peaked, do not hesitate to take contact.
+          </Card.Title>
+          <LinkButton variant="light" to="/info/problem" style={topSpace}>
+            <h4>Contact</h4>
+          </LinkButton>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+const WindTurbineCard = () => {
+  return (
+    <div>
+      <Card className="bg-dark text-white">
+        <Card.Img src={windTurbine} alt="Card image" />
+        <Card.ImgOverlay style={{ marginTop: "130px" }}>
+          <Card.Title>
+            <h3>
               <b>
-                Satellite Energy solutions provides precise electricity price,
-                and wind turbine productions forecasts as well as high
-                resolution data from 1985-2050.
+                Get better data and projections for your renewable business case
+                needs.
               </b>
-            </Card.Text>
-          </Card.ImgOverlay>
-        </Card>
-      </div>
-      <br></br>
-      <br></br>
+            </h3>
+          </Card.Title>
+          <Card.Text>
+            <b>
+              Satellite Energy solutions provides precise electricity price, and
+              wind turbine productions forecasts as well as high resolution data
+              from 1985-2050.
+            </b>
+          </Card.Text>
+        </Card.ImgOverlay>
+      </Card>
+    </div>
+  );
+};
+
+const DiscoverOurServices = () => {
+  return (
+    <>
       <Container>
         <h1>Discover our services</h1>
         <div style={{ width: "700px" }}>
@@ -47,9 +98,53 @@ const HomePage = () => {
           </h6>
         </div>
       </Container>
-      <LinkButton variant="primary" to="/info/problem">
-        Learn more
-      </LinkButton>
+    </>
+  );
+};
+
+const MyFrontPageCardDeck = () => {
+  return (
+    <>
+      <Container>
+        <CardDeck>
+          <Card>
+            <Card.Img variant="top" src={satellite} style={cardImageStyle} />
+            <Card.Body>
+              <Card.Title>Access our satellite data and more</Card.Title>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src={power} style={cardImageStyle} />
+            <Card.Body>
+              <Card.Title>
+                Ready to use electricity price projections
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </CardDeck>
+      </Container>
+      <br />
+      <br />
+      <Container>
+        <CardDeck>
+          <Card>
+            <Card.Img
+              variant="top"
+              src={windTurbineBelow}
+              style={cardImageStyle}
+            />
+            <Card.Body>
+              <Card.Title>Wind Turbine Production Forecasts</Card.Title>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src={screeningMap} style={cardImageStyle} />
+            <Card.Body>
+              <Card.Title>Screening maps of weather or production</Card.Title>
+            </Card.Body>
+          </Card>
+        </CardDeck>
+      </Container>
     </>
   );
 };
