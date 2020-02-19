e404b3af2e357b181e4fc5515c7cb318f84b8cae
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-19 12:58:41 +0100

    Update Collaborators.jsx

--- src/components/collaborators/Collaborators.jsx
+++ src/components/collaborators/Collaborators.jsx
@@ -65,15 +65,11 @@ const Collaborators = () => {
 const CopernicusText = () => {
   return (
     <p>
-      A total of €65bn was invested in 2018 on Europe level by the wind energy
-      sector alone. To accurately facilitate auctions and examine business cases
-      for the more than 420 wind turbines build that year, large amounts of data
-      and analysis is needed. Using the Copernicus data set we aim to provide
-      better forecasts of what production and electricity prices investors and
-      other actors can expect. We will also provide a user-friendly platform to
-      access and retrieve the data the customer needs in a user-friendly format,
-      along with visualized data sets such as screening maps for wind speed or
-      production.
+      The Copernicus Accelerator prepares 50 of Europe’s boldest innovators for
+      scalable success by creating a challenging, inspiring and supportive
+      environment to learn and grow. The 12-month acceleration programme is
+      designed explicitly for driven entrepreneurs and start-ups ready to turn
+      their transformative business cases into impactful commercial solutions.
     </p>
   );
 };
