0c16c8c4e0ba6be06e18ca9699a0c173d056666d
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-06 19:50:42 +0100

    Updates to map and Marker

--- src/components/forecasts/Forecasts.jsx
+++ src/components/forecasts/Forecasts.jsx
@@ -1,26 +1,59 @@
 import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
-import { Map, TileLayer } from "react-leaflet";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import "./forecast.css";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
 
 const Forecasts = () => {
-  const [position, setPosition] = useState("Click map to show lat-lng");
+  const startLocation = [56.25798302076854, 10.667724609375002];
+  const [lat, setLat] = useState(startLocation[0]);
+  const [lng, setLng] = useState(startLocation[1]);
+  const [positionText, setPositionText] = useState("Click map to show lat-lng");
   const handleClick = e => {
-    setPosition(`You clicked:\nLat: ${e.latlng.lat}\nLng: ${e.latlng.lng}`);
+    setLat(e.latlng.lat);
+    setLng(e.latlng.lng);
+    setPositionText(`You clicked:
+    Lat: ${e.latlng.lat}
+    Lng: ${e.latlng.lng}`);
   };
+  const mapOptions = {
+    center: [...startLocation],
+    zoom: 7
+  };
+
+  useEffect(() => {
+    // The following is magic, to allow Marker to work. From: https://github.com/PaulLeCam/react-leaflet/issues/453#issuecomment-541142178
+    const L = require("leaflet");
+
+    delete L.Icon.Default.prototype._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+      iconUrl: require("leaflet/dist/images/marker-icon.png"),
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+    });
+  }, []);
 
   return (
     <>
       <Container>
-        <h3>[UNDER CONSTRUCTION]</h3>
-        <h4>{position}</h4>
-        <Map onClick={handleClick} center={[55.676098, 12.568337]} zoom={15}>
+        <h4>{positionText}</h4>
+        <Map onClick={handleClick} {...mapOptions}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
+          <Marker position={[lat, lng]}>
+            <Popup>
+              Get Data about your Feasibility Study
+              <br />
+              This marker is placed at
+              <br />
+              Lat: {lat}
+              <br />
+              Long: {lng}
+            </Popup>
+          </Marker>
         </Map>
       </Container>
     </>
