c1081d056040d4df3f1a2d2e828a3fc18b8db979
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-09 20:30:00 +0100

    google maps is hard

--- src/components/forecasts/GoogleForecasts.jsx
+++ src/components/forecasts/GoogleForecasts.jsx
@@ -1,35 +1,61 @@
-import React, { Component } from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import React from "react";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+
+const mapStyles = {
+  width: "100%",
+  height: "100%"
+};
+
+const getKey = () => {
+  if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test")
+    return process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+  else if (process.env.NODE_ENV === "production")
+    return "AIzaSyBr6ZNvfbeAgitXdDqV7qF5Q66IydKg1v8";
+};
+
+const key = getKey();
+
+const startCenter = {
+  lat: 55.676098,
+  lng: 12.568337
+};
+
+const startZoom = 14;
+
+const options = {
+  zoomControlOptions: {
+    position: google.maps.ControlPosition.RIGHT_CENTER // ,
+    // ...otherOptions
+  }
+};
 
 const Map = () => {
-  const zoom = 10;
-  const start = {
-    lat: 55.676098,
-    lng: 12.568337
-  };
-  return (
-    <LoadScript
-      id="script-loader"
-      googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY}
-      //   {...other props}
-      language="english"
-    >
-      <GoogleMap
-        id="tradewind-map"
-        //   {...other props }
-      >
-        {/* ...Your map components */}
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: key // ,
+    // ...otherOptions
+  });
+
+  const renderMap = () => {
+    // wrapping to a function is useful in case you want to access `window.google`
+    // to eg. setup options or create latLng object, it won't be available otherwise
+    // feel free to render directly if you don't need that
+    const onLoad = React.useCallback(function onLoad(mapInstance) {
+      // do something with map Instance
+    });
+    return (
+      <GoogleMap options={options} onLoad={onLoad}>
+        {
+          // ...Your map components
+        }
       </GoogleMap>
-    </LoadScript>
-  );
+    );
+  };
+
+  if (loadError) {
+    return <div>Map cannot be loaded right now, sorry.</div>;
+  }
+
+  return isLoaded ? renderMap() : <Spinner />;
 };
 
-const GoogleForecasts = () => {
-  return (
-    <div>
-      <Map />
-    </div>
-  );
-};
-
-export default GoogleForecasts;
+export default Map;
