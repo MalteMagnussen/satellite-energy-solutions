250ca9635afe159070ff3f9dd8b56221a28211c9
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-06-12 16:08:07 +0200

    Update Collaborators site

--- src/components/collaborators/Collaborators.jsx
+++ src/components/collaborators/Collaborators.jsx
@@ -6,7 +6,7 @@ import {
   Card,
   Button,
   // CardGroup,
-  CardColumns
+  CardColumns,
 } from "react-bootstrap";
 import CopernicusMissionPatch from "./presskit/MissionPatch19.png";
 // import ParsecLogo from "./presskit/fb-opengraph.png";
@@ -30,9 +30,7 @@ const Collaborators = () => {
           <Card className="text-center">
             <Card.Body>
               <Card.Title>Copernicus Accelerator</Card.Title>
-              <Card.Text>
-                <CopernicusText />
-              </Card.Text>
+              <CopernicusText />
               <Button
                 variant="primary"
                 href="https://accelerator.copernicus.eu/"
@@ -53,9 +51,7 @@ const Collaborators = () => {
           <Card className="text-center">
             <Card.Body>
               <Card.Title>Parsec Accelerator</Card.Title>
-              <Card.Text>
-                <ParsecText />
-              </Card.Text>
+              <ParsecText />
               <Button
                 variant="primary"
                 href="https://parsec-accelerator.eu/"
@@ -73,9 +69,7 @@ const Collaborators = () => {
           <Card className="text-center">
             <Card.Body>
               <Card.Title>Skylab Ignite</Card.Title>
-              <Card.Text>
-                <IgniteText />
-              </Card.Text>
+              <IgniteText />
               <Button
                 variant="primary"
                 href="https://www.skylab.dtu.dk/programmes-and-services/skylab_ignite"
@@ -102,55 +96,61 @@ const Collaborators = () => {
 
 const CopernicusText = () => {
   return (
-    <p style={align}>
+    <Card.Text style={align}>
       The Copernicus Accelerator prepares 50 of Europe’s boldest innovators for
       scalable success by creating a challenging, inspiring and supportive
       environment to learn and grow. The 12-month acceleration programme is
       designed explicitly for driven entrepreneurs and start-ups ready to turn
       their transformative business cases into impactful commercial solutions.
-    </p>
+    </Card.Text>
   );
 };
 
 const ParsecText = () => {
   return (
-    <>
-      <p style={align}>
-        Tradewind has indirectly received funding from the European Union’s
-        Horizon 2020 research and innovation program, via an Open Call issued
-        and executed under project PARSEC (grant agreement No 824478)
-        <br></br>
-        The holistic two-stage acceleration programme will distribute a total of
-        €2.5 Million equity-free funding to 100 beneficiaries, alongside other
-        key resources (coaching, matchmaking, promotion) needed to develop and
-        launch 15 new products and services into the market. PARSEC will also
-        provide a suite of{" "}
-        <a
-          href="https://parsec-accelerator.eu/business-catalysts/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          business catalysts
-        </a>{" "}
-        for transforming data into market-ready services. The first Open Call
-        for applications was held between 20 September 2019 and 3 January 2020.
-        The second Open Call will be launched in spring 2020.
-      </p>
-    </>
+    <Card.Text style={align}>
+      Tradewind has indirectly received funding from the European Union’s
+      Horizon 2020 research and innovation program, via an Open Call issued and
+      executed under project PARSEC (grant agreement No 824478)
+      <br></br>
+      The holistic two-stage acceleration programme will distribute a total of
+      €2.5 Million equity-free funding to 100 beneficiaries, alongside other key
+      resources (coaching, matchmaking, promotion) needed to develop and launch
+      15 new products and services into the market. PARSEC will also provide a
+      suite of{" "}
+      <a
+        href="https://parsec-accelerator.eu/business-catalysts/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        business catalysts
+      </a>{" "}
+      for transforming data into market-ready services. The first Open Call for
+      applications was held between 20 September 2019 and 3 January 2020. The
+      second Open Call will be launched in spring 2020.
+    </Card.Text>
   );
 };
 
 const IgniteText = () => {
   return (
     <>
-      <p style={align}>
+      <Card.Text style={align}>
         DTU Skylab Ignite is a 10 week early stage startup acceleration program
         suited for students and researchers. The program consists of individual
         1:1 team sessions with professional coaches, network building workshops
         with all participants together, and a final pitching event with 50.000
         DKK in prizes. There is a new round each semester, and each batch has 10
         top motivated high tech (pre)startups.
-      </p>
+        <br /> <br />
+        We are proud to announce that we won third place at the finals, in what
+        was called the best group of candidates since the program began!
+        <br />
+        <a href="https://www.facebook.com/dtuskylab/posts/3784848301587171">
+          Facebook Post announcing Winners.
+        </a>
+        <br />
+      </Card.Text>
     </>
   );
 };
