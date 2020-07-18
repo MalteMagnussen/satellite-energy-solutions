ba5ed4bd6d869f8e977ece7284744e4ef765cd07
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-07-18 14:45:07 +0200

    Removed unused icons from Routes

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,5 @@
-/*!
-
-=========================================================
-* Material Dashboard React - v1.9.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 // @material-ui/icons
-import Dashboard from "@material-ui/icons/Dashboard";
 import PeopleIcon from '@material-ui/icons/People';
-import LibraryBooks from "@material-ui/icons/LibraryBooks";
-import BubbleChart from "@material-ui/icons/BubbleChart";
-import LocationOn from "@material-ui/icons/LocationOn";
 import DesktopMacIcon from '@material-ui/icons/DesktopMac';
 import EcoIcon from '@material-ui/icons/Eco';
 // core components/views for Admin layout
